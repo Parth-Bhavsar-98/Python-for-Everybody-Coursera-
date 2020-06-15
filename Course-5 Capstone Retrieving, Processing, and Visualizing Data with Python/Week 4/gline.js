d840795b9b828cfba6ed7926c1b9eecd937d34be
Parth-Bhavsar-98 <66744436+Parth-Bhavsar-98@users.noreply.github.com>
2020-06-15 22:33:41 +0530

    Add files via upload

---
+++ Course-5 Capstone Retrieving, Processing, and Visualizing Data with Python/Week 4/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','indiana.edu','berkeley.edu','ufp.pt','columbia.edu','uct.ac.za','mac.com','gmail.com','virginia.edu'],
+['2005',57,11,12,12,10,13,14,12,10,9],
+['2006',124,38,36,36,35,31,30,25,27,26]
+];
